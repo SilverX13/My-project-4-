9ab359c1e5751dc0cd47a2b71d77ab12e288e046
SliverX13 <88573307+SilverX13@users.noreply.github.com>
2022-10-30 17:38:38 +0800

    fix bug
    fix bug

--- Assets/Scripts/createene.cs
+++ Assets/Scripts/createene.cs
@@ -10,17 +10,17 @@ public class createene : MonoBehaviour
     void Start()
     {
         gameObjects[0] = Instantiate<GameObject>(games[0]);
-        gameObjects[0].transform.position = new Vector3(-1.235572f, -0.2876849f, 0);
+        gameObjects[0].transform.position = new Vector3(2.18f, -3.55f, 0);
 
         gameObjects[1] = Instantiate<GameObject>(games[1]);
-        gameObjects[1].transform.position = new Vector3(2.47f, 3.17f, 0);
+        gameObjects[1].transform.position = new Vector3(3.21f, -3.55f, 0);
 
 
         gameObjects[2] = Instantiate<GameObject>(games[2]);
-        gameObjects[2].transform.position = new Vector3(2.47f, 3.17f, 0);
+        gameObjects[2].transform.position = new Vector3(4.13f, -3.55f, 0);
 
         gameObjects[3] = Instantiate<GameObject>(games[3]);
-        gameObjects[3].transform.position = new Vector3(2.47f, 3.17f, 0);
+        gameObjects[3].transform.position = new Vector3(3.21f, -2.26f, 0);
     }
 
     // Update is called once per frame
